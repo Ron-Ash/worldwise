6fcde163dd623a47b33ecd295531b782967dbb20
Ron <a158.ron@gmail.com>
2024-08-22 10:03:29 +1000

    added window independent banner

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import Home from "../pages/home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home></Home>} />
-        <Route path="product" element={<Product></Product>} />
-        <Route path="pricing" element={<Pricing></Pricing>} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>This stays no matter which window you go to</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="product" element={<Product></Product>} />
+          <Route path="pricing" element={<Pricing></Pricing>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
