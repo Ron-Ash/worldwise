e740fd7ebb21fcef77f1ab40853c7ff8d2b69983
Ron <a158.ron@gmail.com>
2024-08-22 10:56:16 +1000

    added all starter files from course repo

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,14 +1,20 @@
-function Home() {
+import styles from "./Home.module.css";
+
+export default function Homepage() {
   return (
-    <div>
-      <p>WorldWise</p>
-      <ul>
-        <li>A</li>
-        <li>b</li>
-        <li>c</li>
-      </ul>
-    </div>
+    <main className={styles.home}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+      </section>
+    </main>
   );
 }
-
-export default Home;
