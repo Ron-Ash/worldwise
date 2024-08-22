497d65976a58f1fc445cfdb093719f9e57ce9b8e
Ron <a158.ron@gmail.com>
2024-08-22 10:05:04 +1000

    implement a page not found

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "../pages/Product";
 import Pricing from "../pages/pricing";
 import Home from "../pages/home";
+import NotFound from "../pages/NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home></Home>} />
           <Route path="product" element={<Product></Product>} />
           <Route path="pricing" element={<Pricing></Pricing>} />
+          <Route path="*" element={<NotFound></NotFound>} />
         </Routes>
       </BrowserRouter>
     </div>
