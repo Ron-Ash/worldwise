76230c2b4242cb050b5596413fea5805f3fc84a0
Ron <a158.ron@gmail.com>
2024-08-22 11:08:47 +1000

    added more windows in the page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageNav from "../components/PageNav";
 import Product from "../pages/Product";
 import Pricing from "../pages/pricing";
 import Home from "../pages/home";
 import NotFound from "../pages/NotFound";
-import PageNav from "../components/PageNav";
+import Login from "../pages/Login";
+import Tracking from "../pages/Tracking";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<Home></Home>} />
           <Route path="product" element={<Product></Product>} />
           <Route path="pricing" element={<Pricing></Pricing>} />
+          <Route path="login" element={<Login></Login>} />
+          <Route path="tracking" element={<Tracking></Tracking>} />
           <Route path="*" element={<NotFound></NotFound>} />
         </Routes>
       </BrowserRouter>
