35890b3c1f3a5587db697e3aa3c6e9c871435299
Ron <a158.ron@gmail.com>
2024-08-22 10:20:40 +1000

    added a basic navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Product from "../pages/Product";
 import Pricing from "../pages/pricing";
 import Home from "../pages/home";
 import NotFound from "../pages/NotFound";
+import PageNav from "../components/PageNav";
 
 function App() {
   return (
     <div>
       <h1>This stays no matter which window you go to</h1>
       <BrowserRouter>
+        <PageNav />
+
         <Routes>
           <Route path="/" element={<Home></Home>} />
           <Route path="product" element={<Product></Product>} />
